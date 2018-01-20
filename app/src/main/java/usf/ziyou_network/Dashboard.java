682aaae6d6a31c2f3d0d036a9cf0245adeeafd0d
osnielTAO <osniel.quintana@taoconnect.org>
2018-01-20 16:40:07 -0500

    First push

---
+++ app/src/main/java/usf/ziyou_network/Dashboard.java
@@ -0,0 +1,13 @@
+package usf.ziyou_network;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
